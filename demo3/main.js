050b4bbb9f04f1766c0c69bb619e02ec3e6b8b22
stanislav <stanislavidalgo@gmail.com>
2021-02-23 23:29:00 +0300

    padarau namu darba

---
+++ demo3/main.js
@@ -0,0 +1,15 @@
+document.querySelector('button').addEventListener("click",fn);
+
+function fn() {
+
+const inp = document.querySelector('input');
+const out = document.querySelector('h3');
+
+if (inp.value % 2 === 0){
+    out.textContent = "Yay"
+}else{
+    out.textContent = "Ne"
+}
+}
+
+
