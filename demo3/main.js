1ba615906116e58335f39667cf9adf8e51b3fdc7
stanislav <stanislavidalgo@gmail.com>
2021-02-24 01:11:34 +0300

    suternirinau

--- demo3/main.js
+++ demo3/main.js
@@ -1,15 +1,16 @@
-document.querySelector('button').addEventListener("click",fn);
+document.querySelector("button").addEventListener("click", fn);
 
 function fn() {
+  const inp = document.querySelector("input");
+  const out = document.querySelector("h3");
 
-const inp = document.querySelector('input');
-const out = document.querySelector('h3');
+//   if (inp.value % 2 === 0) {
+//     out.textContent = "Yay";
+//   } else {
+//     out.textContent = "Ne";
+//   }
+// }
 
-if (inp.value % 2 === 0){
-    out.textContent = "Yay"
-}else{
-    out.textContent = "Ne"
+out.textContent = (inp.value % 2 === 0 ? 'yay' : 'ne')
+
 }
-}
-
-
