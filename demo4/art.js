6e2a88849702b0da1a19a1193b00e2dd2ad11512
stanislav <stanislavidalgo@gmail.com>
2021-02-24 20:30:30 +0300

    trikampis

--- demo4/art.js
+++ demo4/art.js
@@ -9,16 +9,24 @@
 // document.body.innerHTML += "L";
 // }
 
+// const number  = 3
 
-const number  = 3
+// for ( let i =0;i < number - 1; i++) {
 
-for ( let i =0;i < number - 1; i++) {
+// document.body.innerHTML += "L<br>";
+// }
+// for ( let i =0;i < number ; i++) {
 
-document.body.innerHTML += "L<br>";
-}
-for ( let i =0;i < number ; i++) {
+// document.body.innerHTML += "L";
+// }
 
-document.body.innerHTML += "L";
+const number = 5;
+
+for (let i = 0; i < number; i++) {
+  for (let a = 0; a < i; a++) {
+    document.body.innerHTML += "L";
+  }
+  document.body.innerHTML += "L<br>";
 }
 
 
