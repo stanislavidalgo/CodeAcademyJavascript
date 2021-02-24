11ebc5273c7e7077a663c7b4e87d5f372620f370
stanislav <stanislavidalgo@gmail.com>
2021-02-24 20:20:49 +0300

    ta pati darau tik su number 3

---
+++ demo4/art.js
@@ -0,0 +1,24 @@
+// const number  = 2
+
+// for ( let i =0;i < number - 1; i++) {
+
+// document.body.innerHTML += "L<br>";
+// }
+// for ( let i =0;i < number ; i++) {
+
+// document.body.innerHTML += "L";
+// }
+
+
+const number  = 3
+
+for ( let i =0;i < number - 1; i++) {
+
+document.body.innerHTML += "L<br>";
+}
+for ( let i =0;i < number ; i++) {
+
+document.body.innerHTML += "L";
+}
+
+
