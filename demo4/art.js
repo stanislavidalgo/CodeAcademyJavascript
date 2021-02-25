53cdcbe86eb7c16f10f38a9c2a3711e288b943bb
stanislav <stanislavidalgo@gmail.com>
2021-02-25 20:37:18 +0300

    piramide

--- demo4/art.js
+++ demo4/art.js
@@ -20,13 +20,12 @@
 // document.body.innerHTML += "L";
 // }
 
-const number = 5;
+// const number = 5;
 
-for (let i = 0; i < number; i++) {
-  for (let a = 0; a < i; a++) {
-    document.body.innerHTML += "L";
-  }
-  document.body.innerHTML += "L<br>";
-}
-
+// for (let i = 0; i < number; i++) {
+//   for (let a = 0; a < i; a++) {
+//     document.body.innerHTML += "L";
+//   }
+//   document.body.innerHTML += "L<br>";
+// }
 
