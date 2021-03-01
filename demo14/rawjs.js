7c1ec68bef053f3e1602e7578d2452f63e5ad800
stanislav <stanislavidalgo@gmail.com>
2021-03-02 00:02:40 +0300

    padarem sekcija

--- demo14/rawjs.js
+++ demo14/rawjs.js
@@ -1,1 +1,18 @@
 document.body.style.backgroundColor = "aqua";
+
+
+const sekcija = document.createElement('main');
+
+// sekcija ===========
+sekcija.style.backgroundColor = "white";
+sekcija.style.maxWidth = "50rem";
+sekcija.style.minHeight = "20rem";
+sekcija.style.textAlign = "center";
+sekcija.style.borderRadius = "1rem";
+sekcija.style.margin = "10rem auto 5rem auto";
+
+
+
+
+
+document.body.prepend(sekcija);
