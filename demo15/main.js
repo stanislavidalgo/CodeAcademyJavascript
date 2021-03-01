0485d9d61a37133d16b94ddca3289f869be64263
stanislav <stanislavidalgo@gmail.com>
2021-03-02 01:07:23 +0300

    surandas info dev.t.baigiam

--- demo15/main.js
+++ demo15/main.js
@@ -3,7 +3,7 @@ const data = JSON.parse(
   );
   
   console.log(data.results)
-  
+
 document.body.style.backgroundColor = "aqua";
 
 
@@ -19,7 +19,7 @@ sekcija.style.margin = "10rem auto 5rem auto";
 
 // nuotrauka ==========
 const image = document.createElement('img');
-image.src = "https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/134989006_5610386142320628_761958712910963655_o.jpg?_nc_cat=101&ccb=3&_nc_sid=09cbfe&_nc_ohc=tYKBZYeyG3YAX8jdhLW&_nc_ht=scontent-arn2-1.xx&oh=cdd4798326daf3ac3ce20d9df0e384a9&oe=606192E6";
+image.src = "https://randomuser.me/api/portraits/med/women/34.jpg";
 image.alt = "stanislav-img"
 image.style.height = "10rem";
 image.style.width = "10rem";
@@ -30,14 +30,18 @@ image.style.marginTop = "-5rem";
 
 // h1================
 const h1 = document.createElement('h1');
-h1.textContent = "Stanislav Idalgo";
+h1.textContent = data.results[0].name.first + " " + data.results[0].name.last;
 
 // p
 
 const adresas = document.createElement('p');
-adresas.textContent = "T.Kosciuskos 26";
+adresas.textContent = data.results[0].location.country + ", " + data.results[0].location.city + ", " + data.results[0].location.street.number + ", " + data.results[0].location.street.name;
+
+// email
+
+const email = document.createElement('p');
+email.textContent = data.results[0].email
 
 
-
-sekcija.append(image, h1, adresas)
+sekcija.append(image, h1, adresas, email)
 document.body.append(sekcija);
