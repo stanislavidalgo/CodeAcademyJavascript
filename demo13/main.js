c049b6a3d4d17b2afeaa77bf67c45994c6ad5d8a
stanislav <stanislavidalgo@gmail.com>
2021-03-01 23:47:02 +0300

    surasom f-ja

--- demo13/main.js
+++ demo13/main.js
@@ -1,2 +1,14 @@
 document.querySelector("form").addEventListener("submit", fn);
 
+
+
+function fn(event) {
+  event.preventDefault();
+
+  const data = document.querySelector("input").value;
+
+  const h1 = document.createElement("h1");
+
+  h1.textContent = data;
+  document.body.append(h1);
+}
