06c745ff56cb254d62a8e20a5ce647515b52bcaf
stanislav <stanislavidalgo@gmail.com>
2021-03-09 00:46:21 +0300

    filtered sizes

--- demo21/main.js
+++ demo21/main.js
@@ -1,5 +1,17 @@
 import data from "./data.js";
 
-const pigiau = data.filter( value => value.price <100);
+// const pigiau = data.filter( value => value.price <100);
 
-console.log(pigiau);
+// console.log(pigiau);
+
+const sizeS = data.filter(value => value.size === "S")
+
+console.log(sizeS);
+
+const sizeM = data.filter(value => value.size === "M")
+
+console.log(sizeM);
+
+const sizeL = data.filter(value => value.size === "L")
+
+console.log(sizeL);
